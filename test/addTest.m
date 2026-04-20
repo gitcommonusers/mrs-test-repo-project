523d2c7c293b1024b7743b61bbd7ee99d1307705
gitcommonusers <128743513+gitcommonusers@users.noreply.github.com>
2026-04-20 12:24:36 -0400

    Refactor add function into addTest class

--- test/addTest.m
+++ test/addTest.m
@@ -1,3 +1,13 @@
-function result = add(a, b)                                                                                   
-      result = a + b;                                                                                           
-  end 
+  classdef addTest < matlab.unittest.TestCase                                                                   
+      methods (Test)                                                                                            
+          function testPositive(testCase)                                                                       
+              testCase.verifyEqual(add(2, 3), 5);                                                               
+          end                                                                                                   
+          function testNegative(testCase)                                                                       
+              testCase.verifyEqual(add(-1, -2), -3);                                                            
+          end                                                                                                   
+          function testZero(testCase)                                                                           
+              testCase.verifyEqual(add(0, 0), 0);                                                               
+          end                                                                                                   
+      end                                                                                                       
+  end  
